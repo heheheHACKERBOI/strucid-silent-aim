6e65766634f68b82cdab0faac6c3133ee48250b8
CriticalHolder <62488209+heheheHACKERBOI@users.noreply.github.com>
2020-12-09 14:25:38 -0800

    Create strucid.lua

---
+++ strucid.lua
@@ -0,0 +1,7 @@
+getgenv().SelectedPart = "Head"
+getgenv().VisibiltyCheck = false
+getgenv().TargetESP = true
+getgenv().FOV = 250
+getgenv().CircleVisibility = true
+getgenv().Distance = 500
+loadstring(game:HttpGet("https://pastebin.com/raw/ef77Q6fU", true))()
